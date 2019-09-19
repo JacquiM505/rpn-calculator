8e7d24a8126e73ba52a45f04babb093deb85cbc6
Jacqueline Martin <jacquimartin505@gmail.com>
2019-09-18 18:56:44 -0600

    TODO Add operate method w/ switch (later version will use @Override)

--- src/edu/cnm/deepdive/Operator.java
+++ src/edu/cnm/deepdive/Operator.java
@@ -9,34 +9,23 @@ public enum Operator {
 
   /** Pops 2 values from stack, pushes sum of the 2 back onto stack. */
   ADD("+"),
-  /** Pops 2 values from stack, pushes differences of the 2 back onto stack. */
+  /** Pops 2 values from stack, pushes difference of the 2 back onto stack. */
   SUBTRACT("-"),
-  /** Pops 2 values from stack, pushes products of the 2 back onto stack. */
+  /** Pops 2 values from stack, pushes product of the 2 back onto stack. */
   MULTIPLY("*"),
-  /** Pops 2 values from stack, pushes times of the 2 back onto stack. */
+  /** Pops 2 values from stack, pushes quotient (real) of the 2 back onto stack. */
   DIVIDE("/"),
+  /** Pops 1 value from stack, pushes its square root back onto stack. */
+  SQUARE_ROOT("sqrt"),
+  /** Pops 2 values from stack, pushes the value of 1st raised to the second back onto stack. */
+  POWER("^"),
+  /** Pops 2 values from stack, pushes remainder after truncated division of the 2 back onto stack. */
+  MODULO("%");
 
-  /** Pops 2 values from stack, pushes
-   SQUARE_ROOT("sqrt"),
-   /* *
-   POWER("^"),
-   /**
-   MODULO("%");
-   /**
+  private String token;
 
-   private String token;
-
-   Operator(String token) {
-   this.token = token;
-   }
-
-
-   /**
-   *
-   * @param args
-   */
-  public static void main(String[] args) {
-    System.out.println(Arrays.toString(Operator.values()));
+  Operator(String token) {
+    this.token = token;
   }
 
   @Override
@@ -44,5 +33,10 @@ public enum Operator {
     return token;
   }
 
-// TODO Add operate method w/ switch (later version will use @Override).
+  public static String tokenPattern() {
+    return "(?:^|\\s)(\\+|\\-|\\*|\\/|\\^|\\%|sqrt)(?:\\s|$)";
+  }
+
+  // TODO Add operate method w/ switch (later version will use @Override).
+
 }
